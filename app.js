65742bf339039b585a41bf6d72707cd36d46cb99
Taha <iamtahamurtaza@gmail.com>
2023-01-31 19:04:11 +0530

    Learned about view engine and working on blog app

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
 const express = require("express");
-
+// express app
 const app = express();
+
+//  register view engine
+app.set("view engine", "ejs");
 
 // listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
   //   res.send("<p>Home page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  //   res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
   //   res.send("<p>About page</p>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  // res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-// redirects
-app.get("/aboutus", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create new page" });
 });
+
+// // redirects
+// app.get("/aboutus", (req, res) => {
+//   res.redirect("/about");
+// });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  // res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.render("404"), { title: "404" };
 });
