9661ef649ad3f1f4595517de1b1ae2a0b0454420
Taha <iamtahamurtaza@gmail.com>
2023-02-01 19:35:26 +0530

    learned about mongo db and working on blog app

--- app.js
+++ app.js
@@ -1,13 +1,83 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+
 // express app
 const app = express();
 
+const dbURI =
+  "mongodb+srv://taha:test1234@nodetuts.zrpozqr.mongodb.net/node-tuts?retryWrites=true&w=majority";
+mongoose
+  .set("strictQuery", false)
+  .connect(dbURI)
+  .then((result) => app.listen(3000), console.log("connected to db"))
+  .catch((err) => console.log(err));
 //  register view engine
 app.set("view engine", "ejs");
 
 // listen for requests
-app.listen(3000);
 
+// middleware
+// app.use((req, res, next) => {
+//   console.log("host", req.host);
+//   console.log("path", req.path);
+//   console.log("method", req.method);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("In the next middleware function");
+//   next();
+// });
+
+// third party middleware
+app.use(morgan("dev"));
+
+// middleware & static files
+app.use(express.static("public"));
+
+//mongoose and mongo sandbox routes
+
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "My Second Blog",
+    snippet: "Creating my Second blog",
+    body: "more content to read from here ...",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// get all the data
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// get a particular blog data
+app.get("/single-blog", (req, res) => {
+  Blog.findById("63da5bdd70a83f9da18fdfec")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// routes
 app.get("/", (req, res) => {
   //   res.send("<p>Home page</p>");
   //   res.sendFile("./views/index.html", { root: __dirname });
